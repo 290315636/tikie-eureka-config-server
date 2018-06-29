5881ec9763bd4babf6aa299682a36b65609ef103
zhaochunsheng <290315636@qq.com>
2018-06-29 15:58:18 +0800

    Initial commit

---
+++ src/main/java/com/tikie/eureka/config/TikieEurekaConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.tikie.eureka.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikieEurekaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikieEurekaConfigServerApplication.class, args);
+	}
+}
